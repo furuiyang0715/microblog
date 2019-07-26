7fef61e4e47b941bfd6028f8773a34d903561e77
furuiyang <furuiyang0715@gmail.com>
2019-07-26 22:27:45 +0800

    初步配置 app

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
-from flask import Flask 
-app = Flask(__name__)
-from app import route
+from flask import Flask
+from config import Config
 
+app = Flask(__name__)
+app.config.from_object(Config)
+
+from app import routes
