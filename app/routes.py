22f33f299de9cea083655781ceebddf81afffbcc
furuiyang <furuiyang0715@gmail.com>
2019-07-27 21:58:14 +0800

    资料修改

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ from werkzeug.urls import url_parse
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
 
-from app.form import LoginForm, RegistrationForm
+from app.form import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 
 
@@ -81,6 +81,23 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.datetime.utcnow()
         db.session.commit()
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
 
 
 @app.route('/logout')
