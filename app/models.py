f28917e2f0b2727ee227485f0e671f3701cf59e3
furuiyang <furuiyang0715@gmail.com>
2019-07-27 17:30:53 +0800

    完成在 shell 中的注册

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from app import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
--- microblog.py
+++ microblog.py
@@ -1,1 +1,10 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+
+# 通过添加数据库实例和一个模型实例来创建一个 shell 上下文环境
+# 当启动 shell 的时候 就会调用这个函数并且在 shell 会话中注册它返回的项目
+# 函数返回一个字典而不是一个列表，原因是对于每个项目，你必须通过字典的键提供一个名称以便在shell中被调用。
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
