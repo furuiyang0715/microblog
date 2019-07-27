2ccb54d4e57486057a672aed68ab508da5fd3612
furuiyang <furuiyang0715@gmail.com>
2019-07-27 18:01:12 +0800

    完善登录视图函数

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
+from flask_login import current_user, login_user
+
 from app import app
 from flask import render_template, flash, redirect, url_for
 
 from app.form import LoginForm
+from app.models import User
 
 
 @app.route("/") 
@@ -23,11 +26,15 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
-    if form.validate_on_submit():  # 将执行表单的校验工作 全部成功返回 True 有失败的就返回 False
-        # 调用 flash 的时候 会将消息存储在 flask 中 但是不会显示出来
-        # 一旦通过get_flashed_messages函数请求了一次，它们就会从消息列表中移除，所以在调用flash()函数后它们只会出现一次。
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for("/index"))
+    if form.validate_on_submit():
+
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
