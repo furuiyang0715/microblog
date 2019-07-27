edb48afef5abbaf7c40b30237cd4864ed46326c3
furuiyang <furuiyang0715@gmail.com>
2019-07-27 16:27:02 +0800

    增加对于数据库的配置

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # 增加 SQLite 数据库的配置
+    # sqlite 数据库比较轻量级
+    # 每个数据库都存在磁盘上的单个文件上面
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    # 设置数据库发生变更时是否发送消息给应用
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
