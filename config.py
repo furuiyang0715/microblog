b232e3c4539cb0c2fd7c6945180d2b6f3b2151c0
furuiyang <furuiyang0715@gmail.com>
2019-07-31 14:16:09 +0800

    little

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # 设置要使用的数据库
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
